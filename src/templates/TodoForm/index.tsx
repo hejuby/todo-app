f65bf63a6838b85a9b7048263776c69ac08caa6c
Marshall Ku <marshall@kakao.com>
2024-04-22 19:30:43 +0900

    Add logics for resetting form after submit

--- src/templates/TodoForm/index.tsx
+++ src/templates/TodoForm/index.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useRef } from "react";
 import { submitTodo } from "app/actions";
 import Button from "#components/Button";
 import Input from "#components/Input";
@@ -8,12 +11,26 @@ import styles from "./index.module.scss";
 const cx = classNames(styles, "todo-form");
 
 function TodoForm() {
+    const formRef = useRef<HTMLFormElement>(null);
+
     return (
-        <form className={cx()} action={submitTodo}>
+        <form
+            className={cx()}
+            action={async (formData) => {
+                await submitTodo(formData);
+                formRef.current?.reset();
+            }}
+            ref={formRef}
+        >
             <Typography variant="h1" component="h1" fontWeight={700}>
                 What&apos;s on your mind?
             </Typography>
-            <Input name="title" placeholder="Add title" label="Todo *" />
+            <Input
+                name="title"
+                placeholder="Add title"
+                label="Todo *"
+                required
+            />
             <Input
                 name="description"
                 placeholder="Add description"
