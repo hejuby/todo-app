58c3fee5fdff6eb824aa7c472c4c51bf603db14e
Marshall Ku <marshall@kakao.com>
2024-04-22 18:01:42 +0900

    Add Input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,24 @@
+import { InputHTMLAttributes, forwardRef } from "react";
+import classNames from "#utils/classNames";
+import styles from "./index.module.scss";
+
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+    label?: string;
+}
+
+const cx = classNames(styles, "input");
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+    ({ label, ...props }, ref) => {
+        return (
+            <label className={cx()}>
+                {label && <span className={cx("__label")}>{label}</span>}
+                <input className={cx("__input")} {...props} ref={ref} />
+            </label>
+        );
+    }
+);
+
+Input.displayName = "Input";
+
+export default Input;
