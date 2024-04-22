14eb5cb1ba58a8f6e4f27a35b43e989c8533dc28
Marshall Ku <marshall@kakao.com>
2024-04-22 18:45:32 +0900

    Modify style of hidden input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -11,7 +11,7 @@ const cx = classNames(styles, "input");
 const Input = forwardRef<HTMLInputElement, InputProps>(
     ({ label, ...props }, ref) => {
         return (
-            <label className={cx()}>
+            <label className={cx("", props.hidden && "--hidden")}>
                 {label && <span className={cx("__label")}>{label}</span>}
                 <input className={cx("__input")} {...props} ref={ref} />
             </label>
