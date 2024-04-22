3f78ec5341fc6183056bdeee2b78b95eceafdc6b
Marshall Ku <marshall@kakao.com>
2024-04-22 17:13:34 +0900

    Add mixins, helpers and global scss files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    output: "standalone",
+    sassOptions: {
+        prependData:
+            '@import "src/styles/abstracts/_variables.scss";@import "src/styles/abstracts/_palette.scss";@import "src/styles/abstracts/_fonts.scss";@import "src/styles/abstracts/_mixins.scss";',
+    },
+};
 
 export default nextConfig;
