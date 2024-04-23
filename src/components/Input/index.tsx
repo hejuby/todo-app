cc362c5e626b755ee663b417040b9e4254cd4a5d
Marshall Ku <marshall@kakao.com>
2024-04-23 10:06:34 +0900

    Add error state in input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,19 +1,31 @@
 import { InputHTMLAttributes, forwardRef } from "react";
+import Typography from "#components/Typography";
 import classNames from "#utils/classNames";
 import styles from "./index.module.scss";
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     label?: string;
+    error?: string;
 }
 
 const cx = classNames(styles, "input");
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
-    ({ label, ...props }, ref) => {
+    ({ label, error, ...props }, ref) => {
+        console.log(props.name, props);
         return (
-            <label className={cx("", props.hidden && "--hidden")}>
+            <label
+                className={cx(
+                    "",
+                    props.hidden && "--hidden",
+                    error && "--error"
+                )}
+            >
                 {label && <span className={cx("__label")}>{label}</span>}
                 <input className={cx("__input")} {...props} ref={ref} />
+                <Typography variant="c2" className={cx("__error")}>
+                    {error}
+                </Typography>
             </label>
         );
     }
